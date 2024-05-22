06a16aa0270f4aced813e385f8c32ddcb9122044
Khaled <me@tuhidulhossain.com>
2024-05-23 02:30:54 +0800

    MarketDataHandler added

---
+++ HFTEngine.MarketDataHandler/MarketDataHandler.cs
@@ -0,0 +1,37 @@
+﻿using HFTEngine.MarketDataHandler.Model;
+using System;
+using WebSocketSharp;
+
+namespace HFTEngine.MarketDataHandler
+{
+    public class MarketDataHandler
+    {
+        private WebSocket webSocket;
+
+        public event Action<MarketData> OnMarketDataReceived;
+
+        public void Connect(string url)
+        {
+            webSocket = new WebSocket(url);
+            webSocket.OnMessage += (sender, e) => ProcessIncomingData(e.Data);
+            webSocket.OnOpen += (sender, e) => Console.WriteLine("Connected to data feed");
+            webSocket.OnError += (sender, e) => Console.WriteLine($"Error: {e.Message}");
+            webSocket.OnClose += (sender, e) => Console.WriteLine("Disconnected from data feed");
+
+            webSocket.Connect();
+        }
+
+        private void ProcessIncomingData(string data)
+        {
+            // Assuming the incoming data is in JSON format
+            var marketData = ParseMarketData(data);
+            OnMarketDataReceived?.Invoke(marketData);
+        }
+
+        private MarketData ParseMarketData(string data)
+        {
+            // Deserialize JSON data to MarketData object (using JSON.NET or similar library)
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<MarketData>(data);
+        }
+    }
+}
---
+++ HFTEngine.MarketDataHandler/Model/MarketData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HFTEngine.MarketDataHandler.Model
+{
+    public class MarketData
+    {
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public int Volume { get; set; }
+    }
+}
